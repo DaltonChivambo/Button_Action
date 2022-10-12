668f32ef117dca74da4db9a15b7f302a5edf85e8
dalton <daltonchivambo@gmail.com>
2022-10-12 15:07:10 +0200

    Adding Event using array

--- app/src/main/java/com/action/buttonaction/MainActivity.java
+++ app/src/main/java/com/action/buttonaction/MainActivity.java
@@ -3,16 +3,15 @@ package com.action.buttonaction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Objects;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvFrases;
-    private Button btnGerar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +21,29 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         tvFrases = findViewById(R.id.tv_frases);
-        btnGerar = findViewById(R.id.btn_gerar);
+        Button btnGerar = findViewById(R.id.btn_gerar);
 
-        btnGerar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String [] arrayFrases = {"aaaa","bbbbb","ccccc"};
-                tvFrases.setText("Welcome");
-            }
+        btnGerar.setOnClickListener(view -> {
+            String phrase1 = "Se o sucesso estiver no cume, escalarei para obtê-lo.\n" +
+                    "Se o sucesso estiver no fundo do oceano, mergulharei para obtê-lo.\n" +
+                    "Se o sucesso estiver no meio da batalha, lutarei por ele.";
+
+            String [] arrayFrases = {
+                    "Comece onde você está, use o que você tem e faça o que você pode.",
+                    "Não importa que você vá devagar, contanto que você não pare.",
+                    "A inspiração existe, porém temos que encontrá-la trabalhando.",
+                    "Conhecer a si mesmo é o começo de toda sabedoria.",
+                    "Descubra quem é você, e seja essa pessoa. A sua alma foi colocada nesse mundo para ser isso, então viva essa verdade e todo resto virá.",
+                    "Acredite em milagres, mas não dependa deles",
+                    "Nossa maior fraqueza está em desistir. O caminho mais certo de vencer é tentar mais uma vez.",
+                    "Mude o modo que você olha para as coisas, e as coisas que você olha mudarão.",
+                    "Toda vez que você se sentir inseguro, relembre as situações em que você se subestimou.",
+                    phrase1
+            };
+
+            Random random = new Random();
+            int randomNumbers = random.nextInt(arrayFrases.length);
+            tvFrases.setText(arrayFrases[randomNumbers]);
         });
     }
 }
